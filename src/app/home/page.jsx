fb14f36d6e6ec7ac3fac762bc51f899eba4e88c9
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2024-06-16 17:48:29 +0545

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -36,7 +36,7 @@ const page = () => {
         end: window.innerHeight,
         onUpdate: (e) => (direction = e.direction * -1),
       },
-      x: "-500px",
+      x: "-300px",
     });
     requestAnimationFrame(animate);
   }, []);
