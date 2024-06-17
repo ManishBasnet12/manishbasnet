ac28a64a63a17dd7c3c716dea3948fd7abe373e6
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2024-06-17 17:28:56 +0545

    update

--- src/app/Components/Service.jsx
+++ src/app/Components/Service.jsx
@@ -7,25 +7,25 @@ const Service = () => {
       heading: "Web Design",
       description:
         "Designing the visual aspects of websites or applications, including layout, color schemes, typography, and overall aesthetics, to enhance usability and user engagement.",
-      icon: "/service1.gif",
+      icon: "/serviceicon1.gif",
     },
     {
       heading: "Web Development",
       description:
         "Creating visually appealing and functional websites using HTML, CSS, and JavaScript to ensure a seamless user experience across different devices and screen sizes.",
-      icon: "/service2.gif",
+      icon: "/serviceicon2.gif",
     },
     {
       heading: "Cross-Browser Compatibility",
       description:
         "They test and optimize websites to work consistently across different web browsers such as Chrome, Firefox, Safari, and Edge",
-      icon: "/service3.gif",
+      icon: "/serviceicon3.gif",
     },
     {
       heading: "Web Animation and Effects",
       description:
         "They create animations, transitions, and special effects using CSS, JavaScript, or libraries like GSAP (GreenSock Animation Platform). These animations enhance user engagement and contribute to a more polished user interface.",
-      icon: "/service4.gif",
+      icon: "/serviceicon4.gif",
     },
   ];
 
