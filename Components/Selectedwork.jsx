283d6a1432f13f949aad8c2260d38023e34f284e
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2024-08-15 21:10:07 +0545

    links

--- Components/Selectedwork.jsx
+++ Components/Selectedwork.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Selectedwork.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const Selectedwork = () => {
   const swork = [
@@ -8,6 +9,7 @@ const Selectedwork = () => {
       sname: "E-Commerce",
       stype: "ui design, animation",
       simage: "/Work/home.jpg",
+      slink:"https://www.behance.net/gallery/195661385/Landing-page-Preloader",
       sdesc:
         "Implement a sleek preloading animation that keeps users engaged while the content loads. Use a simple, branded animation that smoothly transitions into the main content, signaling the page's readiness without causing distractions.",
     },
@@ -15,6 +17,7 @@ const Selectedwork = () => {
       sname: "Subarna Bhandari",
       stype: "Portfolio, Design",
       simage: "/Work/sbhd.png",
+      slink:"https://www.subarnabhandari.com/studio",
       sdesc:
         "Subarna Bhandari's portfolio website features their design projects, showcasing their skills and creative approach. It serves as a professional platform to highlight their expertise and attract potential clients and employers. The site effectively demonstrates their talent and unique style.",
     },
@@ -22,6 +25,7 @@ const Selectedwork = () => {
       sname: "Esabda",
       stype: "web Devlopment, Design",
       simage: "/Work/esabda.png",
+      slink:"https://esabda.com",
       sdesc:
         "The website provides valuable insights and information on Business related topics. It aims to educate readers, offer expert opinions, and keep them informed about industry trends and developments.",
     },
@@ -29,6 +33,7 @@ const Selectedwork = () => {
       sname: "LIMS, DFTQC",
       stype: "Software, Devlopment",
       simage: "/Work/lims.png",
+      slink:"https://lims.dftqc.gov.np",
       sdesc:
         " LIMS improves efficiency by automating sample tracking and workflow management, enhancing data accuracy through centralized data management. It ensures compliance with regulations and provides remote access for better collaboration and scalability.",
     },
@@ -36,6 +41,7 @@ const Selectedwork = () => {
       sname: "Google Traders",
       stype: "Logo, Design",
       simage: "/Work/google.jpg",
+      slink:"https://www.behance.net/gallery/123301413/Gooogle-Traders-Logo",
       sdesc:
         "created and implemented a logo that effectively communicates your brand's identity and enhances your website's appeal.",
     },
@@ -57,6 +63,7 @@ const Selectedwork = () => {
             </div>
           </div>
           <div className="sworkimage">
+            <Link href={work.slink} target="blank">
             <div className="sworkimg">
               <Image
                 src={work.simage}
@@ -66,6 +73,7 @@ const Selectedwork = () => {
                 unoptimized
               />
             </div>
+            </Link>
           </div>
         </div>
       ))}
